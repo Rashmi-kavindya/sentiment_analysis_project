dd045d1e160b4d707c0c87b5de41c95f0cad3b2f
Rashmi-kavindya <kavidissanayaka20@gmail.com>
2025-03-21 11:01:54 +0530

    add bootstrap

--- app.py
+++ app.py
@@ -1,17 +1,35 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
+from helper import preprocessing, vectorizer, get_prediction 
 
 app = Flask(__name__)
 
 data = dict()
-reviews = ['good product', 'Bad product', 'I like it']
-positive = 2
-negative = 1
+reviews = []
+positive = 0
+negative = 0
 @app.route('/')
 def index():
     data['reviews'] = reviews
     data['positive'] = positive
     data['negative'] = negative
     return render_template('index.html', data=data)
+
+@app.route('/', methods = ['POST'])
+def my_post():
+    text = request.form['text']
+    preprossed_text = preprocessing(text)
+    vectorized_text = vectorizer(preprossed_text)
+    prediction = get_prediction(vectorized_text)
+
+    if prediction == 'negative':
+        global negative
+        negative += 1
+    else:
+        global positive
+        positive += 1
+    
+    reviews.insert(0, text)
+    return redirect(request.url)
 
 if __name__ == '__main__':
     app.run()
